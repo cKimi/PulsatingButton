327551b531c8fb94efdcabbb6eded83b012e2b9d
Carlos Kimura <carloskimura@Carloss-MacBook-Pro.local>
2022-11-08 15:40:11 -0300

    pulsating button project

---
+++ PulsatingButton/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  PulsatingButton
+//
+//  Created by Carlos Kimura on 08/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var btn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        btn.layer.cornerRadius = btn.frame.height / 2
+        btn.clipsToBounds = true
+    }
+
+    @IBAction func onClickBtn(_ sender: UIButton) {
+        let pulse = PulseAnimation(numberOfPulses: Float.infinity, radius: 50, position: sender.center)
+        pulse.animationDuration = 1.0
+        pulse.backgroundColor = UIColor.red.cgColor
+        self.view.layer.insertSublayer(pulse, below: self.view.layer)
+    }
+}
+
